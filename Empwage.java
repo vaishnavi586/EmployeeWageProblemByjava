06357055e8a22439c692754acf6f8cee170783d1
vaishnavi586 <machavolu1996@gmail.com>
2020-10-26 23:28:53 +0530

    displayed the status of employee attendance

--- Empwage.java
+++ Empwage.java
@@ -3,6 +3,13 @@ public class Empwage {
       public static void main(String args[]){
 
                  System.out.println("welcome to employee wage computation");
+                 int isfulltime = 1;
+                 double empcheck = Math.floor(Math.random() *10) % 2;
+                 if (empcheck == isfulltime)
+                  System.out.println("employee is present");
+                  else
+                  System.out.println("employee is absent");
+
             }
 
 }
