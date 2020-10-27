1458f9d8b54f98ef359a3c79e121b0615ef4e26c
vaishnavi586 <machavolu1996@gmail.com>
2020-10-27 11:03:28 +0530

    displayed totalsalary of month

--- Empwage.java
+++ Empwage.java
@@ -1,24 +1,30 @@
 public class Empwage {
 
-       void main(String args[]){
+         public static void main(String args[]){
 
                  System.out.println("welcome to employee wage computation");
                  int isfulltime = 1;
                  int isPartTime = 2;
-                 int wagePerHr=20;
+                 int wagePerHr = 20;
+                 int noOfWorkinDays = 20;
+                 int wage=0;
+                  int totalwage=0;
+
+                 for (int day=1; day<=20; day++) {
                  double empcheck = Math.floor(Math.random() *10) % 3;
                   int value = (int)Math.round(empcheck); 
   
                  switch (value) {
                  case 0:
-                   int empHrs = 8;
+                    wage = 8 * 20 ;
+
                    System.out.println("employee is present");
                   
          
 
                     break;               
                   case 1:
-                   int empHrs = 4;
+                   wage = 4*20;
                    System.out.println("employee is present for half time");
 
                     break;
@@ -27,9 +33,10 @@ public class Empwage {
                  
                    break;
             }
-               double salary = empHrs * wagePerHr;
-                     System.out.println("salary" +salary);
+           totalwage= wage + totalwage;
 
+        }
+            System.out.println("total salary is" +totalwage);
 
          }
 }
