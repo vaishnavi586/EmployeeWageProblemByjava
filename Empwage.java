40bda2aed31dab6277e4fad8887b81dec3ccb67f
vaishnavi586 <machavolu1996@gmail.com>
2020-10-27 10:17:44 +0530

    refracted the empwage pb using switch case

--- Empwage.java
+++ Empwage.java
@@ -1,30 +1,35 @@
 public class Empwage {
 
-      public static void main(String args[]){
+       void main(String args[]){
 
                  System.out.println("welcome to employee wage computation");
                  int isfulltime = 1;
                  int isPartTime = 2;
                  int wagePerHr=20;
                  double empcheck = Math.floor(Math.random() *10) % 3;
-                 if (empcheck == isfulltime) {
-                  int empHrs = 8;
-                  System.out.println("employee is present");
-                 
-                  }
-                  else if (empcheck == isPartTime){
-                  int empHrs = 4;
-                  System.out.println("employee is present for half time");
-                  }
+                  int value = (int)Math.round(empcheck); 
+  
+                 switch (value) {
+                 case 0:
+                   int empHrs = 8;
+                   System.out.println("employee is present");
+                  
+         
 
-                  else {
+                    break;               
+                  case 1:
+                   int empHrs = 4;
+                   System.out.println("employee is present for half time");
+
+                    break;
+                 case 2:
                   System.out.println("employee is absent");
-
+                 
+                   break;
             }
+               double salary = empHrs * wagePerHr;
+                     System.out.println("salary" +salary);
 
-                  double salary = empHrs * wagePerHr;
 
-                  System.out.println("salary" +salary);
          }
-
 }
