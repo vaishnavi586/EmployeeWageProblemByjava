602d3a8bb1bb91662a607623036a1e24dfe5d303
vaishnavi586 <machavolu1996@gmail.com>
2020-10-30 22:16:59 +0530

    refracted code and displayed wage for two companies

--- Empwage.java
+++ Empwage.java
@@ -1,11 +1,7 @@
 public class Empwage {
 public static final int IS_PART_TIME = 1;
 public static final int IS_FULL_TIME = 2;
-public static final int EMP_RATE_PER_HOUR = 20;
-public static final int NUM_OF_WORKING_DAYS = 20;
-public static final int MAX_HRS_IN_MONTH = 100;
-
-void wage() {
+public static void wage(String company, int EMP_RATE_PER_HOUR, int MAX_HRS_IN_MONTH, int NUM_OF_WORKING_DAYS) {
 int empHrs = 0; int totalEmpHrs = 0;
  int totalWorkingDays = 0;
 while (totalEmpHrs <= MAX_HRS_IN_MONTH &&  totalWorkingDays < NUM_OF_WORKING_DAYS) {
@@ -27,15 +23,15 @@ System.out.println("Day#: " + totalWorkingDays + " totalEmpHrs: " +totalEmpHrs);
 
 }
 int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-System.out.println( "total employee wage is "+totalEmpWage);
+System.out.println("total employee wage of company" +company+ " is" +totalEmpWage);
 
 }
 
 
 public static void main(String[] args) {
 
-Empwage empobj=new Empwage();
-empobj.wage();
+wage( "DMART", 20, 100, 20);
+wage( "jiomart", 15, 90, 15);
 
 
 }
